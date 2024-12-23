00251a361143b6e25a7248b6c3303b4af1c04afe
Paweł Protaś <pawciopr@gmail.com>
2024-12-23 10:24:56 +0100

    Add implementation of lengthSort function

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -1,6 +1,16 @@
 ﻿#include "sort.hpp"
 
+#include <algorithm>
+
 std::deque<std::string> lengthSort(const std::forward_list<std::string>& input) {
-    std::deque<std::string> output;
+    std::deque<std::string> output{};
+    std::copy(input.begin(), input.end(), std::back_inserter(output));
+    std::sort(output.begin(), output.end(), [](const std::string& lhs, const std::string& rhs) {
+        if (lhs.size() == rhs.size()) {
+            return lhs < rhs;
+        }
+
+        return lhs.size() < rhs.size();
+    });
     return output;
 }
